17265a8725f91086e3008dfc426728dcf6317588
Archana-Shinde1 <Archana.Shinde1@ibm.com>
2024-04-16 10:12:15 +0000

    add build script for default subpackage

---
+++ recipe/build-default.sh
@@ -0,0 +1,8 @@
+cd python/ray/dashboard/client
+npm install
+npm ci
+npm run build
+# not sure why this seems to get copied on windows but not linux...
+mkdir -p $SP_DIR/ray/dashboard/client             # [not win]
+cp -R ./build $SP_DIR/ray/dashboard/client/build  # [not win]
+
